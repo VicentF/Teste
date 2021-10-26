6f3927d904d1dc24dd39baf8e1ba98ecd3cd514e
Vicente <v.felino@campus.fct.unl.pt>
2021-10-26 15:24:13 +0100

    Início

---
+++ src/Teste1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Teste1 {
+     public static void main(String[] args) {
+         System.out.println("teste");
+         System.out.println("Hello world");
+    }
+
+}
